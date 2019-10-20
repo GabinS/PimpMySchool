ba6b8405b2fbaa807b4d7e0f6aff933ecbbec3d7
Damien LASNIER <damien.lasnier@yahoo.fr>
2019-10-20 16:36:59 +0200

    Modification DAOSalleJpa et DAORessourceMaterielleJpa

--- src/main/java/fr/formation/dao/jpa/DAORessourceMaterielleJpa.java
+++ src/main/java/fr/formation/dao/jpa/DAORessourceMaterielleJpa.java
@@ -1,37 +1,10 @@
 package fr.formation.dao.jpa;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import fr.formation.dao.IDAORessourceMaterielle;
 import fr.formation.model.RessourceMaterielle;
 
-public abstract class DAORessourceMaterielleJpa implements IDAORessourceMaterielle {
-	@PersistenceContext
-	private EntityManager em;
-	
-	public RessourceMaterielle findById(int id) {
-		return em.find(RessourceMaterielle.class, id);
-	}
-	
-	public List<RessourceMaterielle> findAll() {
-		return em
-				.createQuery("select r from RessourceMaterielle r", RessourceMaterielle.class)
-				.getResultList();
-	}
-	
-	public RessourceMaterielle save (RessourceMaterielle entity) {
-		entity = em.merge(entity);
-		em.persist(entity);
-		return entity;
-	}
-	
-	public void deleteById(int id) {
-		em
-		.createQuery("delete r from RessourceMaterielle where id = :id")
-		.setParameter("id", id)
-		.executeUpdate();
+public abstract class DAORessourceMaterielleJpa extends DAOJpa<RessourceMaterielle> implements IDAORessourceMaterielle {
+	public DAORessourceMaterielleJpa() {
+		this._Class = RessourceMaterielle.class;
 	}
 }
