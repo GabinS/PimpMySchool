cd68888179a89603777f13fe8ad825aef72866f1
Keromnes Mathieu <mathieu.kero@gmail.com>
2019-10-20 16:24:47 +0200

    Ajout entité Ordinateur (DAO / Model)

--- src/main/java/fr/formation/dao/jpa/DAOOrdinateurJpa.java
+++ src/main/java/fr/formation/dao/jpa/DAOOrdinateurJpa.java
@@ -1,5 +1,39 @@
 package fr.formation.dao.jpa;
 
-public abstract class DAOOrdinateurJpa {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+
+import fr.formation.dao.IDAOOrdinateur;
+import fr.formation.model.Ordinateur;
+
+@Repository
+public abstract class DAOOrdinateurJpa implements IDAOOrdinateur{
+	
+	@PersistenceContext
+	private EntityManager em;
+
+	public Ordinateur FindById(int id) {
+		return em.find(Ordinateur.class, id);
+	}
+
+	public List<Ordinateur> FindAll() {
+		Query myQuery = em.createQuery("select m from Ordinateur m", Ordinateur.class);
+		return myQuery.getResultList();
+	}
+
+	public Ordinateur Save(Ordinateur entity) {
+		entity = em.merge(entity);
+		em.persist(entity);
+		return entity;
+	}
+
+	public void DeleteById(int id) {
+		em.createQuery("delete from Ordinateur where id = :id").setParameter("id", id).executeUpdate();
+	}
 	
 }
--- src/main/java/fr/formation/model/Ordinateur.java
+++ src/main/java/fr/formation/model/Ordinateur.java
@@ -1,5 +1,70 @@
 package fr.formation.model;
 
-public class Ordinateur {
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ordinateur")
+public class Ordinateur extends RessourceMaterielle{
+
+	//Fields
+	@Column(name = "ORD_PROC")
+	private String processeur;
+	
+	@Column(name = "ORD_RAM")
+	private String ram;
+	
+	@Column(name = "ORD_DISQUE")
+	private String disqueDur;
+	
+	@Column(name = "ORD_DATEACHAT")
+	private Date dateAchat;
+
+	
+	//Properties
+	public String getProcesseur() {
+		return processeur;
+	}
+
+	public void setProcesseur(String processeur) {
+		this.processeur = processeur;
+	}
+
+	public String getRam() {
+		return ram;
+	}
+
+	public void setRam(String ram) {
+		this.ram = ram;
+	}
+
+	public String getDisqueDur() {
+		return disqueDur;
+	}
+
+	public void setDisqueDur(String disqueDur) {
+		this.disqueDur = disqueDur;
+	}
+
+	public Date getDateAchat() {
+		return dateAchat;
+	}
+
+	public void setDateAchat(Date dateAchat) {
+		this.dateAchat = dateAchat;
+	}
+
+	//Constructor
+	public Ordinateur() {}
+	
+	public Ordinateur(String processeur, String ram, String disqueDur, Date dateAchat) {
+		super();
+		this.processeur = processeur;
+		this.ram = ram;
+		this.disqueDur = disqueDur;
+		this.dateAchat = dateAchat;
+	}
 }
