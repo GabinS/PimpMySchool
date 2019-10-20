b7f61426644034c2ce8514292bcc9888c2499622
Damien LASNIER <damien.lasnier@yahoo.fr>
2019-10-20 16:36:59 +0200

    Modification DAOSalleJpa et DAORessourceMaterielleJpa

--- src/main/java/fr/formation/dao/jpa/DAOSalleJpa.java
+++ src/main/java/fr/formation/dao/jpa/DAOSalleJpa.java
@@ -1,40 +1,13 @@
 package fr.formation.dao.jpa;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.stereotype.Repository;
 
 import fr.formation.dao.IDAOSalle;
 import fr.formation.model.Salle;
 
 @Repository
-public abstract class DAOSalleJpa implements IDAOSalle {
-	@PersistenceContext
-	private EntityManager em;
-
-	public Salle findById(int id) {
-		return em.find(Salle.class, id);
-	}
-
-	public List<Salle> findAll() {
-		return em
-				.createQuery("select s from Salle s", Salle.class)
-				.getResultList();
-	}
-
-	public Salle save(Salle entity) {
-		entity = em.merge(entity);
-		em.persist(entity);
-		return entity;
-	}
-
-	public void deleteById(int id) {
-		em
-		.createQuery("delete s from Salle where id = :id")
-		.setParameter("id", id)
-		.executeUpdate();
+public abstract class DAOSalleJpa extends DAOJpa<Salle> implements IDAOSalle {
+	public DAOSalleJpa() {
+		this._Class = Salle.class;
 	}
 }
