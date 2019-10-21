93e820de78b86195a26dc085266bfb6b18cbc721
sofia <sofiane.boucard@live.com>
2019-10-21 22:19:36 +0200

    Initialisation de la branche du Vidéoprojecteur
    +Création du modèle du vidéoprojecteur
    +Création d'interface du DAO du Vidéoprojecteur
    +Création du DAO du Vidéoprojecteur

--- src/main/java/fr/formation/dao/jpa/DAOVideoProjecteurJpa.java
+++ src/main/java/fr/formation/dao/jpa/DAOVideoProjecteurJpa.java
@@ -1,5 +1,11 @@
 package fr.formation.dao.jpa;
 
-public abstract class DAOVideoProjecteurJpa {
+import fr.formation.dao.IDAOVideoProjecteur;
+import fr.formation.model.VideoProjecteur;
 
+public abstract class DAOVideoProjecteurJpa extends DAOJpa<VideoProjecteur> implements IDAOVideoProjecteur{
+	public DAOVideoProjecteurJpa() {
+		this._Class = VideoProjecteur.class;
+	}
+	
 }
