f49ccfcb81ed4057d45ec9abf54c4c4843f4a736
emiliego <e.gougeon@iia-laval.fr>
2019-10-21 21:12:00 +0200

    Modification DAO Planning et Model Planning

--- src/main/java/fr/formation/dao/jpa/DAOPlanningJpa.java
+++ src/main/java/fr/formation/dao/jpa/DAOPlanningJpa.java
@@ -1,5 +1,13 @@
 package fr.formation.dao.jpa;
 
-public abstract class DAOPlanningJpa {
-	
+import org.springframework.stereotype.Repository;
+
+import fr.formation.dao.IDAOPlanning;
+import fr.formation.model.Planning;
+
+@Repository
+public abstract class DAOPlanningJpa extends DAOJpa<Planning> implements IDAOPlanning{
+	public DAOPlanningJpa() {
+		this._Class = Planning.class;
+	}
 }
