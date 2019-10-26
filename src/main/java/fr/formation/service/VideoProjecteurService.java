06203482ecc35abd05c7c93bcc415f4c0349064e
sofia <sofiane.boucard@live.com>
2019-10-26 23:39:18 +0200

    +Ajout du Autowired dans le VideoprojecteurService

--- src/main/java/fr/formation/service/VideoProjecteurService.java
+++ src/main/java/fr/formation/service/VideoProjecteurService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import fr.formation.dao.IDAOVideoProjecteur;
@@ -11,6 +12,8 @@ import fr.formation.model.VideoProjecteur;
 
 @Service
 public class VideoProjecteurService {
+	
+	@Autowired(required=false)
 	private IDAOVideoProjecteur daoVideoProjecteur;
 	
 	/**
